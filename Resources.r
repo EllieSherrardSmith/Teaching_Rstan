f9e721913b0a291a13c32bd390b44de03681282d
EllieSherrardSmith <EllieSherrardSmith@users.noreply.github.com>
2019-05-01 07:44:20 +0100

    adding refs

--- Resources.r
+++ Resources.r
@@ -7,7 +7,8 @@
 ## Stan examples
 ## https://mc-stan.org/docs/2_18/stan-users-guide/
 ## https://github.com/stan-dev
-
+## https://mc-stan.org/docs/2_19/functions-reference/skew-normal-distribution.html
+## https://www.metrumrg.com/wp-content/uploads/2018/09/stan_pde_presentation.pdf
 
 ## PKPD models
 
@@ -15,3 +16,4 @@
 ## https://github.com/stan-dev/example-models/tree/feature/issue-72-stan-pkpdlib/misc/pkpd
 ## https://metrumresearchgroup.github.io/Torsten/#2cptmodel
 ## https://github.com/stan-dev/stancon_talks/tree/master/2017/Contributed-Talks/05_margossian/models/effCpt
+
